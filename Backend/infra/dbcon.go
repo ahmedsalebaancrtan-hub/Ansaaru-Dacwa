985c49a3b5ecd286929d6ed40dfe03fda858d807
ahmed saleban <ahmedsalebaancrtan@gmail.com>
2026-07-08 17:05:26 +0300

    completed setup project

---
+++ Backend/infra/dbcon.go
@@ -0,0 +1,24 @@
+package infra
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DbConnect() {
+	config := Configuration
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=disable", config.DbHost, config.DbUser, config.DbPassword, config.DbPort, config.DbName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB = db
+
+}
---
+++ Backend/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/ahmedsaleban/ansaru_dacwa/infra"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	slog.Info("initialised enviroment varibale")
+	infra.InitEnv()
+	config := infra.Configuration
+	slog.Info("Connect database successfully")
+	infra.DbConnect()
+	slog.Info("Connect database succesfully")
+
+	r := gin.Default()
+	slog.Info("application is running successfully on port 9000")
+	r.Run(fmt.Sprintf(":%s", config.Port))
+}
